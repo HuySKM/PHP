5fd1b6cba40ac1f81a76800380b22920f862e5d9
Huy SKM <41476960+HuySKM@users.noreply.github.com>
2018-08-22 16:31:48 +0700

    Update chuvihinhtron.php

--- .gitignore/chuvihinhtron.php
+++ .gitignore/chuvihinhtron.php
@@ -15,7 +15,7 @@ $chieudai =;
 // Chu vi hình tròn
 function chuvihinhtron ($bankinh)
 {
-    $chuvi = ($bankinh * $bankinh) * 3.14;
+    $chuvi = ($bankinh * 2) * 3.14;
     
     return $chuvi;
     
